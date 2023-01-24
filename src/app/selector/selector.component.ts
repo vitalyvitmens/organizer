8b90695496a07e99963e1645ac9562467b17f4ff
vitalyvitmens <otm_@tut.by>
2023-01-24 16:19:21 +0300

    first commit

---
+++ src/app/selector/selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { DateService } from '../shared/date.service';
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.scss'],
+})
+export class SelectorComponent {
+  constructor(public dateService: DateService) {}
+}
---
+++ src/app/shared/date.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DateService {
+  public date: BehaviorSubject<any> = new BehaviorSubject(moment());
+}
